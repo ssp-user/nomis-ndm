c084ac1d05f8960440a36ad404b3fdd58d57a3a6
vishaljoshi <vishaljoshi@N-MacBookPro-08.local>
2021-10-03 22:31:27 +0530

    New change

--- Java-Script/Scoping/script.js
+++ Java-Script/Scoping/script.js
@@ -1,2 +1,16 @@
-i = 10;
-console.log("\n\nValue of i is ", i);
+'use strict';
+function calcAge(birthYear) {
+    const age = 2037 - birthYear;
+
+    // console.log(firstName);
+    function printAge() {
+        const output = "You are " + age + " born in " + birthYear;
+        console.log(output);
+    }
+    printAge();
+    return age;
+}
+
+const firstName = 'Jonas';
+calcAge(1991);
+//console.log("Age is ", calcAge(1991));
